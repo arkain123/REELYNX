7997d9c878bf73001c199a70bef578fb87cd53be
arkain123 <Dimabatkabatka14@gmail.com>
2023-04-12 18:51:19 +0300

    9.1 Бородкин 12.04.2023 18:51

--- Borodkin/Lab9/Lab9_1/Lab9_1.cpp
+++ Borodkin/Lab9/Lab9_1/Lab9_1.cpp
@@ -1,47 +1,90 @@
-﻿#include <stdio.h>
-#include <conio.h>
+﻿#include <iostream>
 
-struct note {
-	char lastname[20];
-	char phone_number[20];
-	int date[3];
-	int salary;
-};
+using namespace std;
+
+void matrix_out(int m[]) {
+	int space = 0;
+	for (int j = 0; j < 100; j++) {
+		space++;
+		cout << m[j] << "\t";
+		if (space >= 10) {
+			space = 0;
+			cout << endl;
+		}
+	}
+	cout << endl << endl;
+}
+
+int oned_to_twod(int m[], int** m2, int rows, int columns) {
+	int row = 0;
+	int column = 0;
+	for (int i = 0; i < (rows * columns * rows); i++) {
+		if (row == rows) {
+			return 0;
+		};
+		if ((row * columns) + column > 99) {
+			m2[row][column] = 0;
+		}
+		else {
+			m2[row][column] = m[(row * columns) + column];
+		}
+		column++;
+		if (column == columns + 1) {
+			row++;
+			column = 0;
+		};
+	}
+}
+
+/*
+void zero_adresses(int(&m2)[10][10]) {
+	cout << "Адреса нулевых элементов двумерной матрицы: " << endl;
+	int n = 0;
+	int* pointer;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (m2[i][j] == 0) {
+				pointer = &m2[i][j];
+				cout << pointer << endl;
+			}
+		}
+	}
+	cout << endl << endl;
+}
+*/
 
 int main() {
-	note workers;
-	FILE* f1;
-	int option = 2;
-	puts("Choose option: ");
-	puts("1. Edit file");
-	puts("2. View file");
-	puts("Any other. Exit program");
-	//scanf("%s", &option);
-	if (option == 1) {
-		f1 = fopen("data.bin", "wb");
-		while (true) {
-			puts(("Input last name or 0 (input ends): "));
-			scanf("%s", &workers.lastname);
-			if (workers.lastname[0] == '0') break;
-			puts(("Input salary: "));
-			scanf("%d", &workers.salary);
-			fwrite(&workers, sizeof(workers), 1, f1);
-		}
-		fclose(f1);
-		puts(("File data.bin edited.\n"));
-		f1 = fopen("data.bin", "r+b");
-		while (true) {
-			fread(&workers, sizeof(workers), 1, f1);
-			if (!feof(f1)) {
-				break;
-			};
-			printf("last name = %s, salary = %d", workers.lastname, workers.salary);
-		}
-		fclose(f1);
+	setlocale(LC_ALL, "ru");
+	system("cls");
+	int m[100] = { 16,  78,  99,   6, -29,  19, -52,  65, -88,  51,
+				  -79, -22,  32, -25, -62, -69,  -2, -59, -75,  89,
+				  -87,  95, -22,  85, -49, -75,  76,  73, -59, -52,
+				   30,  49, -28, -48,   0,  57,  -6, -85,   0, -18,
+				  -97, -21, -95,  64,  22,  -2,  69, -84,  -1, -71,
+				  -25,  47,  72,  43,  15, -44,  44,  61,   4,  74,
+				   88, -61,   0, -64, -83,  97,   0,  90,  15,   8,
+				  -54,  19,  73,  35, -67, -87,  85, -99, -70,  10,
+				   98,  58, -10, -29,  95,  62,  77,  89,  36, -32,
+				   78,  60, -79, -18,  30, -13, -34, -92,   1, -38 };
+
+	matrix_out(m);
+	int rows, columns;
+	cout << "Введите размерность нового массива >" << endl;
+	cout << "i: ";
+	cin >> rows;
+	cout << "j: ";
+	cin >> columns;
+	int** m2;
+	m2 = new int* [rows];
+	for (int i = 0; i < rows; i++) {
+		m2[i] = new int[columns];
 	}
-	else if (option == 2) {
-		f1 = fopen("data.bin", "rb");
-		fread(&workers, sizeof(note), 1, f1);
-		printf("%d %f", workers.lastname, workers.salary);
-	};
+	oned_to_twod(m, m2, rows, columns);
+	
+	for (int i = 0; i < (rows); i++) {
+		for (int j = 0; j < (columns); j++) {
+			cout << m2[i][j] << "\t";
+		}
+		cout << endl;
+	}
 }
